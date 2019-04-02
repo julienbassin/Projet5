22b79ee933e10d9ad27643a0839c3c05291d8ddc
Thierry Chappuis <tchappui@users.noreply.github.com>
2019-04-02 19:32:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from openfoodfacts_requests import OpenFoodFactsRequest
 from openfoodfacts_bdd import openfoodfacts_mysql
 
+# -tc- ne pas mettre de code hors d'une fonction, d'une méthode ou d'une classe, ou exceptionnellement
+
+# -tc- J'utiliserais la recherche par catégories avec tagtype_*, tag_contains_* et tag_*
 PARAMS = {
     "search_terms"  :'riz',
     "search_simple" : '1',
@@ -8,6 +11,7 @@ PARAMS = {
     "json"          : '1'
 }
 
+# -tc- les variables a, b et c ne sont pas descriptives
 a = OpenFoodFactsRequest(PARAMS)
 b = a.Connexion("https://ssl-api.openfoodfacts.org/cgi/search.pl")
 a.parsing_json_object(b)
