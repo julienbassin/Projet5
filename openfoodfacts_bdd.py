0a188ae72ab2adb1056e6a70798b5f9e17846629
Thierry Chappuis <tchappui@users.noreply.github.com>
2019-04-02 19:48:59 +0200

    Update openfoodfacts_bdd.py

--- openfoodfacts_bdd.py
+++ openfoodfacts_bdd.py
@@ -1,7 +1,14 @@
+# -tc- Ajouter une docstring au module
+
 import mysql.connector
 from mysql.connector import errorcode
+# -tc- ne jamais utiliser * dans un import
 from openfoodfacts_requests import *
 
+# -tc- éviter dans la mesure du possible (== pratiquement toujours) les noms de modules avec
+# -tc- des underscores. Utiliser de préférence un seule mot en minuscules.
+
+# -tc- Les noms de classe doivent commencer avec une majuscule et ne doivent pas contenir de _
 class openfoodfacts_mysql:
 
     def __init__(self, user, password, sqlserver):
@@ -23,6 +30,8 @@ class openfoodfacts_mysql:
         try:
             self.conn = mysql.connector.connect(host=self.server,user=self.user,passwd=self.password)
         except mysql.connector.Error as err:
+            # -tc- depuis python 3.6, tu peux remplacer tes format par des fstrings, plus lisible. Eviter les print, préférer
+            # -tc- l'usage de logging qui est fait pour cela.
             print("Something went wrong: {}".format(err))
 
     def create_sql_db(self, database="pur_beurre"):
@@ -31,6 +40,10 @@ class openfoodfacts_mysql:
 
         """
         try:
+            mycursor = self.conn.cursor()
+            # -tc- Ne pas utiliser la concaténation de chaine ou le format pour construire une requête sql. Ou alors il faut 
+            # -tc- s'assurer du contenu de database.
+            # -tc- Laisser à l'utilisateur le soin de créer sa base de base données.
             mycursor = self.conn.cursor()
             mycursor.execute("CREATE DATABASE" + database)
         except mysql.connector.Error as err:
@@ -52,6 +65,7 @@ class openfoodfacts_mysql:
 
         """
         mycursor = self.conn.cursor()
+        # -tc- l'utilisateur que j'utilise pour le tests des projets d'étudiants n'a pas le droit de créer des utilisateurs.
         statement = """CREATE USER 'exemplar'@'localhost' IDENTIFIED BY 'MoreSecurity'"""
         try:
             mycursor.execute(statement)
@@ -60,6 +74,7 @@ class openfoodfacts_mysql:
         except mysql.connector.Error as err:
             print("Something went wrong: {}".format(err))
 
+    # -tc- Beaucoup de méthodes pas nécessairement utiles au projet.
     def info_products_to_bdd(self, products):
             #check if sqlconnexion is not broken
             mycursor = self.conn.cursor()
