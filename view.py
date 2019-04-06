07c83b22bfb93e378c67b07b76db33a8ebc9766d
Silky55 <julienbassin@outlook.com>
2019-04-06 21:21:32 +0200

    update everything plus added code to view py.

--- view.py
+++ view.py
@@ -1,12 +1,16 @@
 import logging
 
+
 class Display:
     """
         Class qui gère l'affichage du menu
     """
 
     def __init__(self):
-        pass
+        self.running = True
 
     def menu(self):
-        pass
+        while self.running:
+            print("1 - Quel aliment souhaitez-vous remplacer ? \
+                2 - Retrouver mes aliments substitués.")
+        #design pattern composite
