ee73a918b7f48565fd3a6bcb17b05643694830c3
julien bassin <julienbassin@outlook.com>
2019-04-23 13:53:57 +0200

    update view.py

--- orchestrator.py
+++ orchestrator.py
@@ -1,5 +1,27 @@
 from view import Tree
 
+import logging
+import json
+from openfoodfacts_requests import OpenFoodFactsRequest
+#from openfoodfacts_bdd import openfoodfacts_mysql
+from config import URL,PARAMS, CAT_PRODUCTS
+
+
+for CatProducts in CAT_PRODUCTS:
+        PARAMS["search_terms"] = CatProducts
+        PARAMS["tag_0"] = CatProducts
+        print(PARAMS["search_terms"])
+
+        request = OpenFoodFactsRequest(PARAMS)
+        final_json = request.Connect(URL)
+        #print(final_json)
+        print(request.GetInfoProducts(final_json))
+
+
+
+
+
+
 a_node = Tree("Vue utilisateur")
 a_node.insert_right("menu-a")
 
