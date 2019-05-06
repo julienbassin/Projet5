33a2facd4703692da44713dce926008f5bb30936
julien bassin <julienbassin@outlook.com>
2019-05-06 19:16:50 +0200

    update everything

--- openfoodfacts_bdd.py
+++ openfoodfacts_bdd.py
@@ -22,6 +22,7 @@ class OpenFoodFactsBdd:
         self.password = password
         self.server = sqlserver
         self.conn = None
+        self.tables = {}
 
     def connect_sql(self):
         """
@@ -33,6 +34,7 @@ class OpenFoodFactsBdd:
             print("connexion bdd successfully ! ")
         except mysql.connector.Error as err:
             logger.error("Something went wrong: {}".format(err))
+            exit(1)
         return self.conn
 
     def request_sql(self, req):
@@ -44,31 +46,59 @@ class OpenFoodFactsBdd:
             logger.error("Something went wrong: {}".format(err))
 
 
+
+    def create_user_sql(self):
+        pass
+
     def create_database(self, database="pur_beurre"):
         """
         Dans create_mysql_db, initialiser les params pour créer la bdd
 
         """
-        sql_database_req = "CREATE DATABASE IF NOT EXISTS {}".format(database)
+        sql_database_req = "CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET 'utf8'".format(database)
         self.request_sql(sql_database_req)
 
     def create_table_products(self):
         """
             Method to create table products
         """
-        pass
+        self.tables['product'] = (
+            "CREATE TABLE `product` ("
+            "  `product_id` int(11) NOT NULL AUTO_INCREMENT,"
+            "  `name` date NOT NULL,"
+            "  `barcode` varchar(14) NOT NULL,"
+            "  `grade` varchar(16) NOT NULL,"
+            "  `website` enum('M','F') NOT NULL,"
+            "  `store` date NOT NULL,"
+            "   `category` varchar(255) NOT NULL,"
+            "  PRIMARY KEY (`product`)"
+            ") ENGINE=InnoDB")
+        self.request_sql(self.tables['product'])
 
     def create_table_categories(self):
         """
             Method to create table categories
         """
-        pass
+        self.tables['category'] = (
+            "CREATE TABLE `category` ("
+            "  `catefory_id` int(11) NOT NULL AUTO_INCREMENT,"
+            "  `name` date NOT NULL,"
+            "   `category` varchar(255) NOT NULL,"
+            "  PRIMARY KEY (`category`)"
+            ") ENGINE=InnoDB")
+        self.request_sql(self.tables['category'])
 
     def create_table_stores(self):
         """
             Method to create table stores
         """
-        pass
+        self.tables['store'] = (
+            "CREATE TABLE `product` ("
+            "  `store_id` int(11) NOT NULL AUTO_INCREMENT,"
+            "  `name` date NOT NULL,"
+            "  PRIMARY KEY (`store`)"
+            ") ENGINE=InnoDB")
+        self.request_sql(self.tables['store'])
 
     def create_tables(self):
         """
