01c57c48bff458a4bba6864233c8d872fe082b24
julien bassin <julienbassin@outlook.com>
2019-05-26 12:51:58 +0200

    openfoodfacts_users.py

---
+++ openfoodfacts_users.py
@@ -0,0 +1,31 @@
+class OpenfoodfactsUsers:
+    """
+        Class which handle any users
+    """
+    def __init__(self):
+        """
+            constructor
+        """
+
+        pass
+
+    def get_all_products(self):
+        """
+            method which get all products from user
+        """
+
+        pass
+
+    def get_favorite_product(self):
+        """
+            Method which get all favorite product from user
+        """
+
+        pass
+
+    def substitute_product(self):
+        """
+            Method which substitute any product from user
+        """
+
+        pass
