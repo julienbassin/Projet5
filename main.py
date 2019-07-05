c7244e1a0a3f7fcb1a12fed4109dd46f2c595722
julien bassin <julienbassin@outlook.com>
2019-07-05 09:13:40 +0200

    add main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from controllers import Controller
+
+def main():
+    """
+        Function to launch the main program
+    """
+    app = Controller()
+    app.run()
+
+if __name__ == "__main__":
+    main()
